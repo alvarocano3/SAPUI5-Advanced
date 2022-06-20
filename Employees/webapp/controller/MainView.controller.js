e015f600e13d278daaf7cb3b526906425672c4e8
alvarocano3 <alvarocano@entrepinares.es>
2022-06-20 14:26:18 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -7,9 +7,20 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("Entrepinares.SAPUI5.employees.controller.MainView", {
-            onInit: function () {
+        var Main = Controller.extend("Entrepinares.SAPUI5.employees.controller.MainView", {});
 
-            }
+            Main.prototype.onValidate = function(){
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee =  inputEmployee.getValue();
+                if ( valueEmployee.length === 6 ){
+                    //inputEmployee.setDescription("OK");
+                    this.getView().byId("labelCountry").setVisible(true);
+                    this.getView().byId("slCountry").setVisible(true);
+                } else{
+                    //inputEmployee.setDescription("Not OK");
+                    this.getView().byId("labelCountry").setVisible(false);
+                    this.getView().byId("slCountry").setVisible(false);                    
+                };                
+            };
+            return Main
         });
-    });
